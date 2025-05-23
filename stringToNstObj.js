9aa97bc2e1faeae7331dd3050cae1542565c4824
zain abbas <zaidizainabbas3@gmail.com>
2025-05-23 15:37:50 +0530

    strNstObj

---
+++ stringToNstObj.js
@@ -0,0 +1,23 @@
+// String to Nested Object
+
+const inpStr = "a.b.c.d";
+
+function unPack(str) {
+    const strArr = str.split(".");
+    let obj = {}
+    let init = ""
+    strArr.forEach((x) => {
+       if(init || Object.keys(obj).length){
+        const n = {[x]:init || obj}
+        init= ""
+        obj = n
+       }else{
+        init = x
+       }
+    });
+
+    return obj
+}
+
+const result = unPack(inpStr)
+console.log(result);
