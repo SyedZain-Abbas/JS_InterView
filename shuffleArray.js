85a59a7db1e948dffabaee408cdddefcfc79ac09
zain abbas <zaidizainabbas3@gmail.com>
2025-06-02 09:09:26 +0530

    shuffle an array

---
+++ shuffleArray.js
@@ -0,0 +1,14 @@
+const cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+
+ shuffle(cards)
+
+ console.log(cards);
+
+function shuffle(arr){
+    for(let i = arr.length -1; i>0; i--){
+        let random = Math.floor(Math.random()*(i+1));
+        [arr[i],arr[random]] = [arr[random],arr[i]]
+    }
+   
+}
+
